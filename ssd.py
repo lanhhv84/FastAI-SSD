d6447a681f492f50d636ed059a7d6adbfcc31fcd
lanhhv84 <lanhhv84@gmail.com>
2019-08-07 13:35:29 +0700

    add test, mobilenetv2ssd

--- ssd.py
+++ ssd.py
@@ -37,10 +37,10 @@ class SSD(nn.Module):
 
         loc_shape = [[bs, 4*self.fm1_priors, 38, 38], \
             [bs, 4*self.fm2_priors, 19, 19], \
-                [4, 4*self.fm3_priors, 10, 10], \
-                    [4, 4*self.fm4_priors, 5, 5], \
-                        [4, 4*self.fm5_priors, 3, 3], \
-                            [4, 4*self.fm6_priors, 1, 1]
+                [bs, 4*self.fm3_priors, 10, 10], \
+                    [bs, 4*self.fm4_priors, 5, 5], \
+                        [bs, 4*self.fm5_priors, 3, 3], \
+                            [bs, 4*self.fm6_priors, 1, 1]
         ]
         conf_shape = [[bs, self.n_classes*self.fm1_priors, 38, 38], \
             [bs, self.n_classes*self.fm2_priors, 19, 19], \
@@ -148,6 +148,9 @@ class VGGSSD(SSD):
 
 
 class MobileNetSSD(SSD):
+    """
+    Single shot multibox object detection with MobileNetSSD
+    """
 
     def __init__(self, n_classes=2):
         super(MobileNetSSD, self).__init__(n_classes=n_classes)
@@ -236,4 +239,106 @@ class MobileNetSSD(SSD):
             nn.ReLU(inplace=True),
         )
 
-    
+
+class MobileNetv2SSD(SSD):
+    """
+    Single shot multibox object detection with MobileNetSSD
+    """
+
+    def __init__(self, n_classes=2):
+        super(MobileNetv2SSD, self).__init__(n_classes=n_classes)
+
+        self.fm1 = nn.Sequential(
+            MobileNetSSD.conv_dw(3, 64, 1, 1),
+            MobileNetSSD.conv_dw(64, 64, 1, 1),
+            nn.MaxPool2d(2, stride=2),
+
+            MobileNetSSD.conv_dw(64, 128, 1, 1),
+            MobileNetSSD.conv_dw(128, 128, 1, 1),
+            nn.MaxPool2d(2, stride=2),
+
+            MobileNetSSD.conv_dw(128, 256, 1, 1),
+            MobileNetSSD.conv_dw(256, 256, 1, 1),
+            MobileNetSSD.conv_dw(256, 256, 1, 1),
+            nn.MaxPool2d(2, stride=2, ceil_mode=True),
+
+            MobileNetSSD.conv_dw(256, 512, 1, 1),
+            MobileNetSSD.conv_dw(512, 512, 1, 1),
+            MobileNetSSD.conv_dw(512, 512, 1, 1)
+        )
+        ## (512, 38, 38)
+        self.fm1_conf = nn.Conv2d(512, n_classes*self.fm1_priors, 3, padding=1)
+        self.fm1_loc = nn.Conv2d(512, 4*self.fm1_priors, 3, padding=1)
+        
+
+        self.fm2 = nn.Sequential(
+            nn.MaxPool2d(2, stride=2),
+            MobileNetSSD.conv_dw(512, 512, 1, 1),
+            MobileNetSSD.conv_dw(512, 512, 1, 1),
+            MobileNetSSD.conv_dw(512, 512, 1, 1),
+            nn.MaxPool2d(3, stride=1, padding=1),
+            MobileNetSSD.conv_dw(512, 1024, 1, 6, 6),
+            MobileNetSSD.create_conv(1024, 1024, 1, stride=1, padding=0)
+        )
+        self.fm2_conf = nn.Conv2d(1024, n_classes*self.fm2_priors, 3, padding=1)
+        self.fm2_loc = nn.Conv2d(1024, 4*self.fm2_priors, 3, padding=1)
+        ## (1024, 19, 19)
+
+        self.fm3 = nn.Sequential(
+            MobileNetSSD.create_conv(1024, 256, 1, padding=0),
+            MobileNetSSD.conv_dw(256, 512, 2, 1),
+        )
+        self.fm3_conf = nn.Conv2d(512, n_classes*self.fm3_priors, 3, padding=1)
+        self.fm3_loc = nn.Conv2d(512, 4*self.fm3_priors, 3, padding=1)
+        ## (512, 10, 10)
+
+        self.fm4 = nn.Sequential(
+            MobileNetSSD.create_conv(512, 128, 1, padding=0),
+            MobileNetSSD.conv_dw(128, 256, 2, 1)
+        )
+        self.fm4_conf = nn.Conv2d(256, n_classes*self.fm4_priors, 3, padding=1)
+        self.fm4_loc = nn.Conv2d(256, 4*self.fm4_priors, 3, padding=1)
+        ## (256, 5, 5)
+
+        self.fm5 = nn.Sequential(
+            MobileNetSSD.create_conv(256, 128, 1, padding=0),
+            MobileNetSSD.conv_dw(128, 256, 1, 0)
+        )
+        self.fm5_conf = nn.Conv2d(256, n_classes*self.fm5_priors, 3, padding=1)
+        self.fm5_loc = nn.Conv2d(256, 4*self.fm5_priors, 3, padding=1)
+        ## (256, 3, 3)
+
+        self.fm6 = nn.Sequential(
+            MobileNetSSD.create_conv(256, 128, 1, padding=0),
+            MobileNetSSD.conv_dw(128, 256, 1, 0)
+        )
+        self.fm6_conf = nn.Conv2d(256, n_classes*self.fm6_priors, 3, padding=1)
+        self.fm6_loc = nn.Conv2d(256, 4*self.fm6_priors, 3, padding=1)
+        ## (256, 1, 1)
+
+        self.fms = [self.fm1, self.fm2, self.fm3, self.fm4, self.fm5, self.fm6]
+        self.fms_conf = [self.fm1_conf, self.fm2_conf, self.fm3_conf, self.fm4_conf, self.fm5_conf, self.fm6_conf]
+        self.fms_loc = [self.fm1_loc, self.fm2_loc, self.fm3_loc, self.fm4_loc, self.fm5_loc, self.fm6_loc]
+
+    @staticmethod
+    def conv_dw(inp, oup, stride, pad, dil=1):
+        return MobileNetv2Block(inp=inp, oup=oup, stride=stride, pad=pad, dil=dil)
+
+class MobileNetv2Block(nn.Module):
+
+    def __init__(self, inp, oup, stride, pad, dil=1):
+        super(MobileNetv2Block, self).__init__()
+
+        self.block = nn.Sequential(
+            nn.Conv2d(inp, inp*6, 1, stride=0, padding=0),
+            nn.ReLU6(inplace=True),
+            nn.Conv2d(inp*6, inp*6, 3, stride=stride, padding=pad, dilation=dil),
+            nn.ReLU6(inplace=True),
+            nn.Conv2d(inp*6, oup, 1, stride=1, padding=0)
+        )
+
+    def forward(self, data):
+        out = self.block(data)
+        if list(out.shape) == list(data.shape):
+            return out + data
+        return out
