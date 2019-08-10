e10860aede542b0551a4471f1aabf6a995fb9346
lanhhv84 <lanhhv84@gmail.com>
2019-08-10 17:07:58 +0700

    optimize iou_table function

--- ssd.py
+++ ssd.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import numpy as np
 
 
 class SSD(nn.Module):
@@ -237,3 +238,215 @@ class MobileNetSSD(SSD):
         )
 
     
+
+
+
+def iou_table(x, truth, bs, nboxes, maxlen, im_size, eps = 1e-3):
+    ious = torch.zeros(bs, nboxes, maxlen) # (bs, 8732, maxlen)
+    pred_xA = (x[:, 0, :] - x[:, 2, :]/2).contiguous().view(bs, nboxes)[..., None].expand(bs, nboxes, maxlen)
+    pred_yA = (x[:, 1, :] - x[:, 3, :]/2).contiguous().view(bs, nboxes)[..., None].expand(bs, nboxes, maxlen)
+    pred_xB = (x[:, 0, :] + x[:, 2, :]/2).contiguous().view(bs, nboxes)[..., None].expand(bs, nboxes, maxlen)
+    pred_yB = (x[:, 1, :] + x[:, 3, :]/2).contiguous().view(bs, nboxes)[..., None].expand(bs, nboxes, maxlen)
+
+    truth_xA = (truth[:, 0, :] - truth[:, 2, :] / 2).contiguous().view(bs, maxlen)[:, None, :].expand(bs, nboxes, maxlen)
+    truth_yA = (truth[:, 1, :] - truth[:, 3, :] / 2).contiguous().view(bs, maxlen)[:, None, :].expand(bs, nboxes, maxlen)
+    truth_xB = (truth[:, 0, :] + truth[:, 2, :] / 2).contiguous().view(bs, maxlen)[:, None, :].expand(bs, nboxes, maxlen)
+    truth_yB = (truth[:, 1, :] + truth[:, 3, :] / 2).contiguous().view(bs, maxlen)[:, None, :].expand(bs, nboxes, maxlen)
+
+
+
+
+    xA = torch.max(pred_xA, truth_xA)
+    yA = torch.max(pred_yA, truth_yA)
+    xB = torch.min(pred_xB, truth_xB)
+    yB = torch.min(pred_yB, truth_yB)
+       
+        
+        
+    interX = torch.clamp(xB - xA + 1/im_size, min=0)
+    interY = torch.clamp(yB - yA + 1/im_size, min=0)
+    interArea =  interX* interY
+    
+
+    boxAArea = (pred_xB - pred_xA + 1/im_size) * (pred_yB - pred_yA + 1/im_size)
+    boxBArea = (truth_xB - truth_xB + 1/im_size) * (truth_yB - truth_yA + 1/im_size)
+    ious = interArea / torch.clamp(boxAArea + boxBArea - interArea, min=eps) # [bs, 8732, maxlen]
+    return ious
+
+
+
+def ssd_loss(out, truth_loc, truth_conf, smoothl1, cre, device, n_classes, im_size, iou_thres=0.6, eps = 1e-3):
+    loc, conf = out
+    assert len(loc) == 6
+    assert len(conf) == 6
+    assert len(loc[0].shape) == 4 
+    bs = truth_loc.shape[0]
+    maxlen = truth_loc.shape[1]
+    truth = truth_loc.permute(0, 2, 1) # (bs, 4, maxlen)
+    # offset (calculate center)
+    truth = (truth + 1)*(im_size/2)
+    truth[:, 0, :], truth[:, 1 ,:] = truth[:, 1 ,:], truth[:, 0 ,:]
+    truth[:, 2, :], truth[:, 3, :] = truth[:, 3, :] - truth[:, 0, :], truth[:, 2, :] - truth[:, 1 ,:]
+    truth[:, 0, :], truth[:, 1 ,:] = truth[:, 0, :] + 0.5*truth[:, 2, :], truth[:, 1 ,:] + 0.5*truth[:, 3, :]
+    
+    
+    truth = truth/im_size
+    
+    
+    for i in range(len(loc)):
+        assert torch.sum(torch.isnan(loc[i])) == 0, "loc is NaN"
+        loc[i] = loc[i].reshape(bs, -1, 4, loc[i].shape[-1], loc[i].shape[-1])
+        conf[i] = conf[i].reshape(bs, -1, n_classes, loc[i].shape[-1], loc[i].shape[-1])
+        
+    """
+        loc:
+            - (4, 4, 38, 38)
+            - (6, 4, 19, 19)
+            - (6, 4, 10, 10)
+            - (6, 4, 5, 5)
+            - (4, 4, 3, 3)
+            - (4, 4, 1, 1)
+            
+        conf:
+            - (4, n_classes, 38, 38)
+            - (6, n_classes, 19, 19)
+            - (6, n_classes, 10, 10)
+            - (6, n_classes, 5, 5)
+            - (4, n_classes, 3, 3)
+            - (4, n_classes, 1, 1)
+        truth_loc: (bs, maxlen, 4)
+        truth_conf: (bs, maxlen)
+    """
+    
+    
+    ratios4 = [1, 2, 0.5, 1]
+    ratios6 = [1, 2, 0.5, 3, 1/3, 1]
+    
+    ratios_map = [ ratios4, ratios6, ratios6, ratios6, ratios4, ratios4]
+    scales = [0.1, 0.2, 0.375, 0.55, 0.725, 0.9]
+    
+    pred = []
+    priors = []
+    
+    for index in range(len(loc)):
+        prior = torch.zeros_like(loc[index]) # bs, -1, 4, loc[i].shape[-1], loc[i].shape[-1]
+        # calculate prior (cx, cy)
+        for row in range(prior.shape[-1]):
+            for col in range(prior.shape[-1]):
+                prior[..., 0, row, col] = (0.5 + row) / prior.shape[-1] # Prior cx
+                prior[..., 1, row, col] = (0.5 + col) / prior.shape[-1] # prior cy
+        # calculate prior (w, h)
+        ratios = ratios_map[index]
+        scale = scales[index]
+        for i in range(len(ratios)):
+            r = ratios[i]
+            if i == len(ratios) - 1:
+                scale = scale*1.5
+            w = scale*np.sqrt(r)
+            h = scale/np.sqrt(r)
+            # w
+            prior[..., i, 2, :, :]  = w/prior.shape[-1]
+            prior[..., i, 3, :, :] = h/prior.shape[-1]
+        # done prior
+        # cx
+        assert torch.sum(torch.isnan(prior)) == 0, "prior is NaN"
+        pred_box = torch.zeros_like(loc[index])
+        pred_box[...,0,:,:] = loc[index][...,0,:,:]*prior[...,2,:,:] + prior[...,0,:,:]
+        pred_box[...,1,:,:] = loc[index][...,1,:,:]*prior[...,3,:,:] + prior[...,1,:,:]
+        pred_box[...,2,:,:] = torch.exp(loc[index][...,2,:,:])*prior[...,2,:,:]
+        pred_box[...,3,:,:] = torch.exp(loc[index][...,3,:,:])*prior[...,3,:,:]
+        priors.append(prior)
+        pred_box = pred_box.permute(0, 2, 1, 3, 4).contiguous().view(bs, 4, -1)
+        pred_box = torch.clamp(pred_box, min=0, max=1)
+        assert torch.sum(torch.isnan(pred_box)) == 0, "Pred is NaN"
+        pred.append(pred_box)
+    
+    pred = torch.cat(pred, dim=2) # (bs, 4, 8732)
+    assert list(pred.shape) == [bs, 4, 8732]
+    
+    assert list(truth.shape) == [bs, 4, maxlen]
+    # IoU
+    ious = iou_table(pred, truth=truth, bs=bs, nboxes=pred.shape[-1], im_size=im_size, maxlen=maxlen)
+    pconf = torch.cat([c.permute(0, 1, 3, 4, 2).contiguous().view(bs, -1, n_classes) for c in conf], dim=1)
+    assert list(pconf.shape) == [bs, 8732, n_classes]
+    max_ious, max_match = torch.max(ious, dim=2)
+    positive_match = max_ious > iou_thres # (bs, 8732)
+    assert list(positive_match.shape) == [bs, 8732]
+    pos_pred = pred.permute(0, 2, 1)[positive_match] # (total_match in batch, 4)
+    # done
+    # 443 -> 4, 4, 3, 8732
+    tr = truth[..., None].expand(*truth.shape, 8732).permute(0, 3, 2, 1) # 4, 8732, 3, 4
+    assert list(tr.shape) == [bs, 8732, maxlen, 4]
+    
+    
+    
+    
+    
+    
+    truth_mask = torch.zeros_like(ious).byte().view(-1, maxlen) # (bs, 8732, maxlen)
+    truth_mask.scatter_(1, max_match.view(-1)[:, None], 1.)
+    truth_mask = truth_mask.view(bs, -1, maxlen)
+    
+    assert torch.sum(truth_mask).item() == 8732*bs, "Total number of item is {}".format(torch.sum(truth_mask).item())
+    
+    # pass
+    truth_mask = positive_match[..., None]*truth_mask
+    tr = tr[truth_mask]
+    
+    
+    assert list(tr.shape) == list(pos_pred.shape)
+    loc_loss = smoothl1(pos_pred, tr)
+    # confidence loss
+    """
+    
+    
+    """
+    assert list(pconf.shape) == [bs, 8732, n_classes]
+    
+    
+    
+    priors = torch.cat([c.permute(0, 1, 3, 4, 2).contiguous().view(bs, -1, 4) for c in priors], dim=1)
+    assert list(priors.shape) == [bs, 8732, 4]
+    priors = priors.permute(0, 2, 1)
+    truth_ious = iou_table(priors, truth=truth, bs=bs, im_size=im_size, nboxes=pred.shape[-1], maxlen=maxlen)
+
+    max_iou, max_index = torch.max(truth_ious, dim=-1)# [bs, 8732], [bs, 8732]
+    # index: (bs, 8732) value (bs, 8732, maxlen)
+    truth_conf = truth_conf[:, None, :].expand(bs, 8732, maxlen) # (bs, 8732, maxlen)
+    assert list(truth_conf.shape) == [bs, 8732, maxlen]
+    tcof = truth_conf.gather(2, max_index[..., None].to(device))
+    tcof[max_iou < iou_thres] = 0
+    assert list(tcof.shape) == [bs, 8732, 1], "Got shape {}".format(list(tcof.shape))
+    """
+    Use truth_ious as a mask
+    """
+    
+    tcof = tcof.view(bs, -1)
+    
+    npos = torch.clamp(torch.sum(tcof > 0), min=1e-3)
+    nneg = 3*npos
+    
+    pos_index = tcof > 0
+    neg_index = tcof == 0
+    if npos > eps:
+        conf_pos_loss = cre(pconf[pos_index].view(-1, n_classes), tcof[pos_index].view(-1))
+    else:
+        conf_pos_loss = 0
+    if nneg >= 1:
+        
+        p = pconf[neg_index].view(-1, n_classes)
+        t = tcof[neg_index].view(-1, 1)
+        
+        maxp = torch.max(p, dim=1)[0]
+        
+        max_error = torch.topk(maxp, int(nneg.item()), dim=0)[1]
+        p = p[max_error, :]
+        t = t[max_error, :]
+        
+        conf_neg_loss = cre(p, t.view(-1))
+    else:
+        conf_neg_loss = 0
+    
+    conf_loss = conf_pos_loss + conf_neg_loss
+    
+    return (loc_loss/torch.clamp(torch.sum(truth_mask), min=eps) + conf_loss/npos)
